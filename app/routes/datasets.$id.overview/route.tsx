ca8e63eb43363c970b00b7dec3e8d494d833130c
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2024-10-21 10:45:24 -0400

    update dataset routes

--- app/routes/datasets.$id.overview/route.tsx
+++ app/routes/datasets.$id.overview/route.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderFunction } from '@remix-run/node';
-import { Outlet, useLoaderData } from '@remix-run/react';
+import { Outlet, useLoaderData, useOutletContext } from '@remix-run/react';
 import { getDatasets, initializeDatabase } from '../../db/db';
 import { useParams } from '@remix-run/react';
 import { DatasetType } from '../../db/types';
@@ -7,15 +7,6 @@ import { Text } from '@radix-ui/themes';
 import React from 'react';
 import PageHeader from '~/components/Structural/Headers/PageHeader';
 import { Button } from '~/components/ui/button';
-export const clientLoader = async () => {
-  const datasets = await getDatasets();
-
-  return { datasets };
-};
-
-interface LoaderData {
-  datasets: DatasetType[];
-}
 
 const datasetUseCases = [
   {
@@ -50,28 +41,14 @@ const datasetUseCases = [
 ];
 
 export default function DatasetIndex() {
-  const { datasets } = useLoaderData<LoaderData>();
-  const params = useParams();
-  const id = params.id;
-
-  const thisDataset = datasets.find(
-    (dataset: DatasetType) => dataset.id.toString() === id
-  );
-
-  if (!thisDataset) {
-    return (
-      <div>
-        <PageHeader title="Dataset Not Found" />
-        <p>The requested dataset could not be found.</p>
-      </div>
-    );
-  }
+  const thisDataset = useOutletContext<DatasetType>();
 
   return (
     <div className="flex flex-row w-full h-full">
       <div className="flex flex-col px-6 py-8 grow *:w-full *:max-w-[1400px] *:mx-auto">
-        <div className="rounded-md border border-base p-8 w-full">
+        <div className="rounded-md border border-base p-8 w-full flex flex-col gap-4">
           <Text className="leading-none text-lg font-medium ">Summary</Text>
+          {thisDataset.description}
         </div>
       </div>
       <div className="flex flex-col w-1/5 bg-deep border-l border-base p-6 space-y-8">
--- app/routes/datasets.$id.preview/route.tsx
+++ app/routes/datasets.$id.preview/route.tsx
@@ -1,38 +1,12 @@
 import { json, LoaderFunction } from '@remix-run/node';
 import { Outlet, useLoaderData } from '@remix-run/react';
 import { getDatasets, initializeDatabase } from '../../db/db';
-import { useParams } from '@remix-run/react';
+import { useParams, useOutletContext } from '@remix-run/react';
 import { DatasetType } from '../../db/types';
 import React from 'react';
-import PageHeader from '~/components/Structural/Headers/PageHeader';
-
-export const clientLoader = async () => {
-  const datasets = await getDatasets();
-
-  return { datasets };
-};
-
-interface LoaderData {
-  datasets: DatasetType[];
-}
 
 export default function DatasetIndex() {
-  const { datasets } = useLoaderData<LoaderData>();
-  const params = useParams();
-  const id = params.id;
+  const thisDataset = useOutletContext<DatasetType>();
 
-  const thisDataset = datasets.find(
-    (dataset: DatasetType) => dataset.id.toString() === id
-  );
-
-  if (!thisDataset) {
-    return (
-      <div>
-        <PageHeader title="Dataset Not Found" />
-        <p>The requested dataset could not be found.</p>
-      </div>
-    );
-  }
-
-  return <div>Overview </div>;
+  return <div>Preview {thisDataset.name}</div>;
 }
