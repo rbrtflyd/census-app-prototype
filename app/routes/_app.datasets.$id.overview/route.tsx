09b033a3013e5ce7dfed13c3b2f965d8291942c2
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2024-11-07 11:06:30 -0500

    update main overview

--- app/routes/_app.datasets.$id.overview/route.tsx
+++ app/routes/_app.datasets.$id.overview/route.tsx
@@ -39,19 +39,71 @@ const datasetUseCases = [
     action: 'Enrich',
   },
 ];
+const issues = [
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+  {
+    title: 'Issue 1',
+    description: 'Description 1',
+  },
+];
 
 export default function DatasetIndex() {
   const thisDataset = useOutletContext<DatasetType>();
 
   return (
-    <div className="flex flex-row w-full h-full">
-      <div className="flex flex-col px-6 py-8 grow *:w-full *:max-w-[1400px] *:mx-auto">
-        <div className="rounded-md border border-base p-8 w-full flex flex-col gap-4">
-          <Text className="leading-none text-lg font-medium ">Summary</Text>
-          {thisDataset.description}
+    <div className="flex flex-col w-full h-full">
+      <div className="flex  px-6 py-8 bg-subtle">
+        <div className="flex flex-row gap-6 justify-between items-center w-full max-w-[1400px] mx-auto">
+          <div className="flex flex-col gap-2 items-start ">
+            <Text className="text-2xl font-medium">{thisDataset.name}</Text>
+            <div className="flex flex-row gap-2">
+              <Button variant="secondary">Edit</Button>
+              <Button variant="secondary">Delete</Button>
+            </div>
+          </div>
+          <div className="rounded-md border border-base p-8 flex flex-col gap-4 bg-white w-1/3">
+            <div className="flex flex-row justify-between items-center *:leading-none">
+              <Text className=" font-medium ">Issues</Text>
+              <Text className="text-light">{issues.length} issues</Text>
+            </div>
+            <div className="flex flex-col gap-2">
+              {issues.map((issue) => (
+                <button
+                  key={issue.title}
+                  className="p-2 rounded-md hover:bg-subtle text-left">
+                  {issue.title}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col min-w-1/5 w-1/3 max-w-[500px] bg-subtle border-l border-base p-6 space-y-8 overflow-hidden">
+      <div className="flex flex-col px-6 py-8 grow *:w-full *:max-w-[1400px] *:mx-auto gap-6">
+        <div className="rounded-md border border-base p-8 w-full flex flex-col gap-4">
+          <Text className="leading-none text-lg font-medium ">Definition</Text>
+          <div className="h-[300px] w-full bg-subtle rounded-md" />
+        </div>
+      </div>
+      {/* <div className="flex flex-col min-w-1/5 w-1/3 max-w-[500px] bg-subtle border-l border-base p-6 space-y-8 overflow-hidden">
         <Text className="text-lg font-medium">
           Things to do with this dataset
         </Text>
@@ -64,7 +116,7 @@ export default function DatasetIndex() {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
