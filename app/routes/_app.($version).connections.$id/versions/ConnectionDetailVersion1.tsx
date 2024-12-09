5e98de34ab1fa356e9e302c3733b4fe3d8ab7916
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2024-12-09 10:33:23 -0500

    add tabs to detail v1

--- app/routes/_app.($version).connections.$id/versions/ConnectionDetailVersion1.tsx
+++ app/routes/_app.($version).connections.$id/versions/ConnectionDetailVersion1.tsx
@@ -9,6 +9,7 @@ import { useBreadcrumbContext } from '~/providers/breadcrumbContext';
 import { format } from 'date-fns';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGauge, faPencil, faTrash } from '@fortawesome/pro-solid-svg-icons';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 
 export default function ConnectionDetailVersion1({
   version,
@@ -41,35 +42,8 @@ export default function ConnectionDetailVersion1({
           thisWorkspaceConnection.name
             ? thisWorkspaceConnection.name
             : thisConnection.connectionServiceName
-        }
-      />
-
-      <main className="h-full px-6 overflow-y-auto *:mx-auto *:w-full *:max-w-[1400px]">
-        <div className="flex flex-row gap-4 py-6 border-b border-slate-50 justify-between">
-          <div className="flex flex-row items-center gap-2">
-            <div className="size-10 flex items-center justify-center border border-base rounded-md bg-white mr-2 shadow-sm">
-              <img
-                src={thisConnection.logo}
-                alt={thisConnection.connectionServiceName}
-                className="size-6"
-              />
-            </div>
-            {thisWorkspaceConnection.name ? (
-              <Text className="text-lg font-medium">
-                {thisWorkspaceConnection.name}
-              </Text>
-            ) : (
-              <Text className="text-lg font-medium">
-                {thisConnection.connectionServiceName}
-              </Text>
-            )}
-            <Badge>
-              <div className="w-2 h-2 rounded-full bg-green-500 mr-1" />
-              <Text className="capitalize">
-                {thisWorkspaceConnection.lastTestStatus}
-              </Text>
-            </Badge>
-          </div>
+        }>
+        <PageHeader.RightSlot>
           <div className="flex flex-row items-center gap-4">
             <Button
               variant="secondary"
@@ -99,20 +73,72 @@ export default function ConnectionDetailVersion1({
               Delete
             </Button>
           </div>
-        </div>
-        <div className="flex flex-row gap-6 py-4">
-          {metaInfo.map((info) => (
-            <div className="flex flex-row gap-2">
-              <div className="flex flex-col gap-2">
-                <Text className="font-medium">{info.label}</Text>
-              </div>
-              <Text>{info.value}</Text>
+        </PageHeader.RightSlot>
+      </PageHeader>
+      <main className="h-full overflow-y-auto *:mx-auto *:w-full *:max-w-[1400px]">
+        <div className="flex flex-row gap-4 py-6 px-6  justify-between items-center">
+          <div className="flex flex-row items-center gap-2">
+            <div className="size-10 flex items-center justify-center border border-base rounded-md bg-white mr-2 shadow-sm">
+              <img
+                src={thisConnection.logo}
+                alt={thisConnection.connectionServiceName}
+                className="size-6"
+              />
             </div>
-          ))}
+            {thisWorkspaceConnection.name ? (
+              <Text className="text-lg font-medium">
+                {thisWorkspaceConnection.name}
+              </Text>
+            ) : (
+              <Text className="text-lg font-medium">
+                {thisConnection.connectionServiceName}
+              </Text>
+            )}
+            <Badge>
+              <div className="w-2 h-2 rounded-full bg-green-500 mr-1" />
+              <Text className="capitalize">
+                {thisWorkspaceConnection.lastTestStatus}
+              </Text>
+            </Badge>
+          </div>
+
+          <div className="flex flex-row gap-6 py-4">
+            {metaInfo.map((info) => (
+              <div className="flex flex-col gap-2 text-xs leading-none">
+                <Text className="font-medium">{info.label}</Text>
+                <Text>{info.value}</Text>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="h-full w-full bg-slate-75 rounded-lg p-4 flex items-center justify-center">
-          <Text>So much space for possibilities</Text>
-        </div>
+
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="details">
+              <Text>Details</Text>
+            </TabsTrigger>
+
+            <TabsTrigger value="settings">
+              <Text>Settings</Text>
+            </TabsTrigger>
+            <TabsTrigger value="syncs">
+              <Text>Syncs</Text>
+            </TabsTrigger>
+            <TabsTrigger value="datasets">
+              <Text>Datasets</Text>
+            </TabsTrigger>
+            <TabsTrigger value="projects">
+              <Text>Projects</Text>
+            </TabsTrigger>
+          </TabsList>
+          <div className="px-6 h-full">
+            <TabsContent value="details">
+              <div className="h-full w-full bg-slate-75 rounded-lg p-4 flex items-center justify-center">
+                <Text>So much space for possibilities</Text>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
       </main>
     </div>
   );
