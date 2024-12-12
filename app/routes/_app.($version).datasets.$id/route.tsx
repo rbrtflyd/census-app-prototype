5df7ba1865356d6417de42700e3563b2e4b9102d
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2024-12-12 16:46:43 -0500

    update enhance home page

--- app/routes/_app.($version).datasets.$id/route.tsx
+++ app/routes/_app.($version).datasets.$id/route.tsx
@@ -237,25 +237,54 @@ export default function DatasetIndex() {
                   />
                 </Button>
               </DrawerTrigger>
-              <DrawerContent direction="right">
-                <DrawerHeader>
-                  <Text className="font-medium text-lg">Enrich & Enhance</Text>
-                  <Button variant="ghost">
-                    <FontAwesomeIcon icon={faTimes} />
-                  </Button>
+              <DrawerContent
+                direction="right"
+                className="overflow-hidden">
+                <DrawerHeader className="bg-gradient-to-tr from-pink-500 to-plum-500 text-white p-12">
+                  <div className="flex flex-row gap-4 items-center">
+                    <FontAwesomeIcon
+                      icon={faSparkles}
+                      className="text-[28px] text-white"
+                    />
+                    <div className="flex flex-col gap-2 leading-none">
+                      <Text className="font-medium text-2xl">
+                        Enrich & Enhance
+                      </Text>
+                      <Text>
+                        Enrich and enhance this dataset which is great for
+                        reasons
+                      </Text>
+                    </div>
+                  </div>
+                  <DrawerClose asChild>
+                    <Button
+                      variant="ghost"
+                      size="icon">
+                      <FontAwesomeIcon icon={faTimes} />
+                    </Button>
+                  </DrawerClose>
                 </DrawerHeader>
                 <div className="flex flex-col gap-2 p-6">
-                  <button className="flex items-center gap-2 p-6 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium">Enrich Company</Text>
+                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
+                    <Text className="font-medium text-lg">Enrich Company</Text>
                   </button>
-                  <button className="flex items-center gap-2 p-6 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium">Use AI</Text>
+                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
+                    <Text className="font-medium text-lg">Use AI</Text>
                   </button>
-                  <button className="flex items-center gap-2 p-6 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium">Calculate and Reference</Text>
+                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
+                    <Text className="font-medium text-lg">
+                      No Code Calculations
+                    </Text>
                   </button>
-                  <button className="flex items-center gap-2 p-6 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium">Dedupe</Text>
+                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
+                    <Text className="font-medium text-lg">
+                      Reference Related Data
+                    </Text>
+                  </button>
+                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
+                    <Text className="font-medium text-lg">
+                      Deduplicate Data
+                    </Text>
                   </button>
                 </div>
               </DrawerContent>
