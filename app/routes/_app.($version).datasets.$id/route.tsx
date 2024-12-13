2e1918238384b41fc5eb5460e9cc4d52ee85c243
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2024-12-13 10:41:20 -0500

    add contexts for enrich and enhance drawer

--- app/routes/_app.($version).datasets.$id/route.tsx
+++ app/routes/_app.($version).datasets.$id/route.tsx
@@ -72,6 +72,7 @@ import { useBreadcrumbContext } from '~/providers/breadcrumbContext';
 import { Button } from '~/components/ui/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Badge } from '~/components/ui/badge';
+import { EnrichEnhanceDrawer } from './EnrichEnhanceHub';
 
 export const clientLoader = async ({
   params,
@@ -225,70 +226,7 @@ export default function DatasetIndex() {
                 </div>
               </PopoverContent>
             </Popover>
-            <Drawer direction="right">
-              <DrawerTrigger asChild>
-                <Button
-                  size="small"
-                  variant="fancy">
-                  <Text>Enrich & Enhance</Text>{' '}
-                  <FontAwesomeIcon
-                    icon={faCaretDown}
-                    className="text-xxs ml-2"
-                  />
-                </Button>
-              </DrawerTrigger>
-              <DrawerContent
-                direction="right"
-                className="overflow-hidden">
-                <DrawerHeader className="bg-gradient-to-tr from-pink-500 to-plum-500 text-white p-12">
-                  <div className="flex flex-row gap-4 items-center">
-                    <FontAwesomeIcon
-                      icon={faSparkles}
-                      className="text-[28px] text-white"
-                    />
-                    <div className="flex flex-col gap-2 leading-none">
-                      <Text className="font-medium text-2xl">
-                        Enrich & Enhance
-                      </Text>
-                      <Text>
-                        Enrich and enhance this dataset which is great for
-                        reasons
-                      </Text>
-                    </div>
-                  </div>
-                  <DrawerClose asChild>
-                    <Button
-                      variant="ghost"
-                      size="icon">
-                      <FontAwesomeIcon icon={faTimes} />
-                    </Button>
-                  </DrawerClose>
-                </DrawerHeader>
-                <div className="flex flex-col gap-2 p-6">
-                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium text-lg">Enrich Company</Text>
-                  </button>
-                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium text-lg">Use AI</Text>
-                  </button>
-                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium text-lg">
-                      No Code Calculations
-                    </Text>
-                  </button>
-                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium text-lg">
-                      Reference Related Data
-                    </Text>
-                  </button>
-                  <button className="flex items-center gap-2 p-8 rounded-lg hover:bg-deep border border-base">
-                    <Text className="font-medium text-lg">
-                      Deduplicate Data
-                    </Text>
-                  </button>
-                </div>
-              </DrawerContent>
-            </Drawer>
+            <EnrichEnhanceDrawer />
           </PageHeader.RightSlot>
         </PageHeader>
         <Tabs
