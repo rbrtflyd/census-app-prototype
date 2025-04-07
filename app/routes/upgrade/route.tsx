2515a6393ae7d982c133738a11e4fa06eba3aa8c
Robert Floyd <67548487+rbrtflyd@users.noreply.github.com>
2025-04-07 13:59:02 -0400

    update upgrade

--- app/routes/upgrade/route.tsx
+++ app/routes/upgrade/route.tsx
@@ -487,7 +487,7 @@ export default function BillingUpgrade() {
               </div>
             </div>
           </div>
-          <div className="w-full p-4 bg-subtle rounded flex flex-col gap-2 items-start">
+          <div className="w-full p-4 rounded-lg flex flex-col gap-2 items-start bg-gradient-to-br from-[#0A0254] from-20% to-plum-500">
             <Text>Need more destinations, SSO, or Audience Hub?</Text>
             <Button
               variant="ghost"
