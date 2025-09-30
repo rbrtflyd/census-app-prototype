e450c90a4d245ae57044a25149d20369799b1e0d
Robert Floyd <rbrt@hey.com>
2025-09-30 10:48:56 -0400

    update layouts

--- app/routes/_app.($version).settings.user/route.tsx
+++ app/routes/_app.($version).settings.user/route.tsx
@@ -2,7 +2,7 @@ import PageHeader from '~/components/Structural/Headers/PageHeader';
 
 export default function SettingsStorage() {
   return (
-    <div className="h-full flex flex-col w-full mx-auto max-w-[1400px] gap-8">
+    <div className="h-full flex flex-col w-full">
       <PageHeader title="User Settings" />
     </div>
   );
