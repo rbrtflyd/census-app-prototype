9944e04e7cdc5d839994392b7747fef1ed24400e
Robert Floyd <rbrt@hey.com>
2025-09-30 10:30:50 -0400

    add other settings routes besides organization

---
+++ app/routes/_app.($version).settings.user/route.tsx
@@ -0,0 +1,9 @@
+import PageHeader from '~/components/Structural/Headers/PageHeader';
+
+export default function SettingsStorage() {
+  return (
+    <div className="h-full flex flex-col w-full mx-auto max-w-[1400px] gap-8">
+      <PageHeader title="User Settings" />
+    </div>
+  );
+}
